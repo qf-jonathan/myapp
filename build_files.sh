686f2bb39b172baf9985163d3f8050d5d18b5ef8
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2023-09-11 01:59:16 -0500

    temporal credentials

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input
 python3.9 manage.py migrate
+
+
+DJANGO_SUPERUSER_USERNAME=testuser \
+DJANGO_SUPERUSER_PASSWORD=testpass \
+DJANGO_SUPERUSER_EMAIL="admin@admin.com" \
+python3.9 manage.py createsuperuser --noinput
